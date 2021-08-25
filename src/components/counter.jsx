e078ea6c8fd615cd939753d3b4811acff3e144af
valcaro87@gmail.com <valcaro87@gmail.com>
2021-08-25 13:49:02 +0800

    passing handling props, states,  and components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: this.props.value
+        count: this.props.counter.value
         //tags: ['tag1','tag2','tag3']
     };
 
@@ -42,6 +42,8 @@ class Counter extends Component {
                     Increment++
                 
                 </button>
+
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2"> Delete </button>
                 
                 {/* <ul>
                     {this.state.tags.map(tag=> <li key={tag}>{ tag }</li>)}
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,17 +9,35 @@ class Counters extends Component {
             { id: 2, value: 4},
             { id: 3, value: 0},
             { id: 4, value: 0}
-        ]
+        ],
+        zzz: "val"
     };
+
+    handleDelete = (counterID) => {
+        // console.log('delete called', counterID);
+        const counters = this.state.counters.filter(c => c.id !== counterID);
+        const id_deleted = counterID
+        this.setState({counters});
+        this.setState({zzz: "deleted: " + id_deleted});
+    };
+
     render() { 
         return ( 
             <div>
                 { this.state.counters.map (counter => (
-                    <Counter key={counter.id} value={counter.value} id={counter.id} selected={true}>
+                    <Counter 
+                        key={counter.id} 
+                        onDelete={this.handleDelete} 
+                        // value={counter.value} 
+                        // id={counter.id} 
+                        // selected={true}
+                        counter={counter}
+                    >
                        
                     </Counter> 
                 
                 ))}
+                <h1>{this.state.zzz}</h1>
             </div> 
         );
     }
