e11b07a0f4869bdec3790b97fc530fba3ce4be91
valcaro87@gmail.com <valcaro87@gmail.com>
2021-09-11 21:38:39 +0800

    multiple components sync with parent and children

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,54 +3,21 @@ import Counter from './counter';
 
 
 class Counters extends Component {
-    state = {  
-        counters: [
-            { id: 1, value: 0},
-            { id: 2, value: 4},
-            { id: 3, value: 0},
-            { id: 4, value: 0}
-        ],
-        zzz: "0 deletions"
-    };
-
-    handleIncrement = (counter) => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = { ...counter };
-        counters[index].value++;
-        // console.log(counters[index]);
-        this.setState({ counters });
-    };
-
-    handleDelete = (counterID) => {
-        // console.log('delete called', counterID);
-        const counters = this.state.counters.filter(c => c.id !== counterID);
-        const id_deleted = counterID
-        this.setState({counters});
-        this.setState({zzz: "deleted id: " + id_deleted});
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({ counters });
-    };
+    
 
     render() { 
         return ( 
             <div>
                 <button
-                    onClick={this.handleReset}
+                    onClick={this.props.onReset}
                     className="btn btn-primary btn-sm m-2">
                     Reset
                 </button>
-                { this.state.counters.map (counter => (
+                { this.props.counters.map (counter => (
                     <Counter 
                         key={counter.id} 
-                        onDelete={this.handleDelete} 
-                        onIncrement={this.handleIncrement}
+                        onDelete={this.props.onDelete} 
+                        onIncrement={this.props.onIncrement}
                         // value={counter.value} 
                         // id={counter.id} 
                         // selected={true}
@@ -60,7 +27,7 @@ class Counters extends Component {
                     </Counter> 
                 
                 ))}
-                <h1>{this.state.zzz}</h1>
+                {/* <h1>{this.props.zzz}</h1> */}
             </div> 
         );
     }
